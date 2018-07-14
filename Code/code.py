33fdeac9a718e7cb450fdf75693f95d990b8d993
Akshay993 <akshaytotal1993@gmail.com>
2018-07-15 02:54:32 +0530

    gradient descent working but inefficient.. need to add more features or use a advance optimization

--- Code/code.py
+++ Code/code.py
@@ -8,16 +8,45 @@ Created on Thu Jul  5 17:32:39 2018
 import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import normalize
 
 
 
-def cost_function(X,Theta,y):
+def gradient_descent(X, theta, y, alpha, max_iterations):
+    
+    final_theta=np.zeros(X.shape[1])
+    final_theta=final_theta.reshape(X.shape[1],1)
+    
+    iterations=[]
+    c=[]
+    
+    #Iterating max_iteration times to find the best theta    
+    for i in range(max_iterations):
+        
+        z=np.dot(X,final_theta)
+        hx= sigmoid(z)
+        dz= hx-y
+        grad= np.dot(np.transpose(X),dz)/X.shape[0]
+        #grad= (1/m).*(X'*(hx-y)        
+        
+        final_theta= np.subtract(final_theta, alpha * grad)
+        iterations.append(i)
+        k=cost_function(X,final_theta,y)
+        c.append(k)
+        
+
+        
+    return final_theta
+        
+
+
+def cost_function(X,theta,y):
     
     #Calculating h(x)
-    z=np.dot(X,Theta)
+    z=np.dot(X,theta)
     hx=sigmoid(z)
     cost= np.dot(np.transpose(-y),np.log(hx)) - np.dot((np.transpose(1 - y)), np.log(1 - hx))
-    return cost/X.shape[1]
+    return cost/X.shape[0]
     
     
     
@@ -55,12 +84,27 @@ test_y=np.asarray(test_y)
 train_x = np.hstack([np.ones([train_x.shape[0],1]), train_x])
 test_x = np.hstack([np.ones([test_x.shape[0],1]), test_x])
 
+
+train_x=normalize(train_x, axis=0, norm='l1')
+test_x=normalize(test_x, axis=0, norm='l1')
+
+
 #Parameter vector theta
-Theta=np.zeros(train_x.shape[1])
+initial_theta=np.zeros(train_x.shape[1])
 
-Theta=Theta.reshape(train_x.shape[1],1)
+initial_theta=initial_theta.reshape(train_x.shape[1],1)
 
-cost=cost_function(train_x,Theta,train_y)
+#Finding the initial error/cost 
+cost = cost_function(train_x,initial_theta,train_y)
+
+
+#Now, we will implement Gradient Descent to find the Optimal Parameters for the model
+final_theta = gradient_descent(train_x, initial_theta, train_y, alpha = 1000.9, max_iterations = 50000)
+
+
+cost1 = cost_function(test_x,final_theta,test_y)
+
+print('hello')
 
 
 
