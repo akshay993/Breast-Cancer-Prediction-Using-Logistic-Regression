a7a9d465a1d1a819dd5c928c17183663232a94d5
Akshay993 <akshaytotal1993@gmail.com>
2018-07-22 18:49:57 +0530

    Created seperate functions for different tasks. Now getting resonable accuracy on both training and test set

--- Code/code.py
+++ Code/code.py
@@ -1,110 +1,181 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-"""
-Created on Thu Jul  5 17:32:39 2018
 
-@author: akshaychopra
-"""
 import pandas as pd
 import numpy as np
+
+from matplotlib import pyplot as plt
 from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import normalize
 
 
 
-def gradient_descent(X, theta, y, alpha, max_iterations):
+def init():
     
-    final_theta=np.zeros(X.shape[1])
-    final_theta=final_theta.reshape(X.shape[1],1)
+    df=pd.read_csv("../Data/data.csv")
     
-    iterations=[]
-    c=[]
     
-    #Iterating max_iteration times to find the best theta    
-    for i in range(max_iterations):
+    #Dropping the 'id' column from the dataset
+    df=df.drop("id",1)
+    df=df.drop("Unnamed: 32",1)
+    
+    #Mapping M to 1 and B to 0 in the output Label DataFrame
+    df['diagnosis']=df['diagnosis'].map({'M':1,'B':0})
+    
+    
+    #Split Data into training and test (70% and 30%)
+    train, test = train_test_split(df, test_size = 0.3, random_state=1)
         
-        z=np.dot(X,final_theta)
-        hx= sigmoid(z)
-        dz= hx-y
-        grad= np.dot(np.transpose(X),dz)/X.shape[0]
-        #grad= (1/m).*(X'*(hx-y)        
+    
+    #Training Data
+    train_x=train.loc[:,'radius_mean' : 'fractal_dimension_worst']
+    train_y=train.loc[:,['diagnosis']]
+    
+    #Testing Data
+    test_x=test.loc[:,'radius_mean' : 'fractal_dimension_worst']
+    test_y=test.loc[:,['diagnosis']]
+    
+    #Converting Traing and Test Data to numpy array
+    train_x=np.asarray(train_x)
+    train_y=np.asarray(train_y)
+    test_x=np.asarray(test_x)
+    test_y=np.asarray(test_y)
+    
+    #Calling the model function to train a Logistic Regression Model on Training Data
+    d= model(train_x.T, train_y.T, num_of_iterations=10000, alpha=0.000001)
+    
         
-        final_theta= np.subtract(final_theta, alpha * grad)
-        iterations.append(i)
-        k=cost_function(X,final_theta,y)
-        c.append(k)
-        
+    costs=d["costs"]
+    w=d["w"]
+    b=d["b"]
+    
+    #Drawing the plot between cost and number of iterations
+    plt.plot(costs)
+    plt.title("Cost vs #Iterations")
+    plt.xlabel("Number of Iterations ( * 10)")
+    plt.ylabel("Cost")
+    
+    
+    #Now, calculating the accuracy on Training and Test Data
+    Y_prediction_train = predict(train_x.T, w, b)
+    Y_prediction_test = predict(test_x.T, w, b)
+    
+    
+    
+    print("\nTrain accuracy: {} %".format(100 - np.mean(np.abs(Y_prediction_train - train_y.T)) * 100))
+    
+    print("\nTest accuracy: {} %".format(100 - np.mean(np.abs(Y_prediction_test - test_y.T)) * 100))
+    
+    
+    
+    
 
-        
-    return final_theta
-        
-
-
-def cost_function(X,theta,y):
+#Function to initialize the weights and bias
+def initialize(m):
     
-    #Calculating h(x)
-    z=np.dot(X,theta)
-    hx=sigmoid(z)
-    cost= np.dot(np.transpose(-y),np.log(hx)) - np.dot((np.transpose(1 - y)), np.log(1 - hx))
-    return cost/X.shape[0]
+    w = np.zeros((m,1))
+    b = 0
     
+    return w , b
     
-    
-    
+#Function to calculate sigmoid of x    
 def sigmoid(X):
-    return 1/(1 + np.exp(- X))
-
-df=pd.read_csv("../Data/data.csv")
-
-#Dropping the 'id' column from the dataset
-df=df.drop("id",1)
-df=df.drop("Unnamed: 32",1)
-
-#Mapping M to 1 and B to 0 in the output Label DataFrame
-df['diagnosis']=df['diagnosis'].map({'M':1,'B':0})
-
-#Split Data into training and test (70% and 30%)
-train, test = train_test_split(df, test_size = 0.3)
-
-#Training Data
-train_x=train.loc[:,['texture_mean','perimeter_mean','smoothness_mean','compactness_mean','symmetry_mean']]
-train_y=train.loc[:,['diagnosis']]
-
-#Testing Data
-test_x=test.loc[:,['texture_mean','perimeter_mean','smoothness_mean','compactness_mean','symmetry_mean']]
-test_y=test.loc[:,['diagnosis']]
-
-#Converting Traing and Test Data to numpy array
-train_x=np.asarray(train_x)
-train_y=np.asarray(train_y)
-test_x=np.asarray(test_x)
-test_y=np.asarray(test_y)
-
-#Appending column of 1 to train_x and test_x for bias term
-train_x = np.hstack([np.ones([train_x.shape[0],1]), train_x])
-test_x = np.hstack([np.ones([test_x.shape[0],1]), test_x])
+    return 1/(1 + np.exp(- X))    
 
 
-train_x=normalize(train_x, axis=0, norm='l1')
-test_x=normalize(test_x, axis=0, norm='l1')
+#Function for doing forward and back propogation
+def propogate(X, Y, w, b):
+    
+    m = X.shape[1] #Number of training examples
+
+    #Forward Propogation, calculating the cost
+    Z = np.dot(w.T, X) + b;    
+    A = sigmoid(Z)
+    cost= -(1/m) * np.sum(Y * np.log(A) + (1-Y) * np.log(1-A))
+    
+    #Back Propogation , calculating the gradients
+    dw = (1/m)* np.dot(X, (A-Y).T)
+    db = (1/m)* np.sum(A-Y)
+    
+    grads= {"dw" : dw, "db" : db}
+    
+    return grads, cost
+    
+    
 
 
-#Parameter vector theta
-initial_theta=np.zeros(train_x.shape[1])
+#Function for performing Grdient Descent
+def optimize(X, Y, w, b, num_of_iterations, alpha):
+    
+    costs=[] 
+    
+    for i in range(num_of_iterations):
+ 
+        grads, cost = propogate(X, Y, w, b)
+        
+        dw = grads["dw"]
+        db = grads["db"]
+        
+        w = w - alpha * dw
+        b = b - alpha * db
+        
+        #Storing tthe cost at interval of every 10 iterations
+        if i% 10 == 0:
+            costs.append(cost)
+            print("cost after %i iteration : %f" % (i, cost))
+            
+            
+    parameters = {"w":w, "b":b}
+    grads = {"dw":dw, "db":db}
+    
+    
+    return parameters, grads, costs
 
-initial_theta=initial_theta.reshape(train_x.shape[1],1)
 
-#Finding the initial error/cost 
-cost = cost_function(train_x,initial_theta,train_y)
+#Function for doing the predictions on the data set (mapping probabilities to 0 or 1)
+def predict(X, w, b):
+    
+    m = X.shape[1] #Number of training examples
+    
+    y_prediction =  np.zeros((1,m))
+    
+    w = w.reshape(X.shape[0], 1)
+    
+    A=sigmoid(np.dot(w.T, X)+b)
+    
+    
+    for i in range(A.shape[1]):
+        
+        if(A[0,i]<0.5):
+            y_prediction[0,i]=0
+        else:
+            y_prediction[0,i]=1
+            
+    
+    return y_prediction
 
 
-#Now, we will implement Gradient Descent to find the Optimal Parameters for the model
-final_theta = gradient_descent(train_x, initial_theta, train_y, alpha = 1000.9, max_iterations = 50000)
+#Function for calculating the Logistic Regression Model
+def model(Xtrain, Ytrain, num_of_iterations, alpha):
+    
+    dim = Xtrain.shape[0] #Number of features
+    
+    w,b = initialize(dim)
+    
+    parameters, grads, costs = optimize(Xtrain, Ytrain, w, b, num_of_iterations, alpha) 
+    
+    w = parameters["w"]
+    b = parameters["b"]
+        
+    
+    d={"w":w, "b":b, "costs": costs}
+    
+    return d
+    
 
 
-cost1 = cost_function(test_x,final_theta,test_y)
+#Calling the init function to start the program
+init()
 
-print('hello')
 
 
 
